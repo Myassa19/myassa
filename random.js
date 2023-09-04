a8e03883bc098bd080ef9aada394b9af6b78a30c
Myassa maallem <129672284+Myassa19@users.noreply.github.com>
2023-09-04 22:11:07 +0000

    Add files via upload

---
+++ random.js
@@ -0,0 +1,10 @@
+const toggleBtn = document.querySelector('.toggle_btn')
+const toggleBtnIcon = document.querySelector('toggle_btn i')
+const dropDownMenu = document.querySelector('.dropdown')
+ toggleBtn.onclick = function(){
+    dropDownMenu.classList.toggle('open')
+    const isOpen = dropDownMenu.classList.contains('open')
+    toggleBtnIcon.classList= isOpen
+    ? 'fa_solid fa-xmark'
+    : 'fa_solid fa-bars'
+ }
